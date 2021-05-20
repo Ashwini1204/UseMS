9d6e8892a62dc39c2702ca4a10c7dde4e56d0d8a
Ashwini <ashwini12vk@gmail.com>
2021-05-20 19:25:17 +0530

    first commit msg

---
+++ src/main/java/com/infy/infycart/user/InfyshopUserMsApplication.java
@@ -0,0 +1,16 @@
+package com.infy.infycart.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class InfyshopUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyshopUserMsApplication.class, args);
+	}
+
+}
